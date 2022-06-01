be94332ccb996f96121823351ad7c8dc85e6f659
harshitsutariya0143@gmail.com <harshitsuthariya0143@gmail.com>
2022-06-01 19:34:00 -0400

    lab 1 completed

---
+++ bash/sysinfo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#displaying the hostname information
+hostnamectl
+echo "Hostname: pc200464301"
+#displaying the domain name
+domainname
+#displaying the IP addresses
+hostname -I
+#displaying the storage information
+df -h
