45aaf9deb4ec0d0f82279e226af7d7afcaa4ffdf
harshitsutariya0143@gmail.com <harshitsuthariya0143@gmail.com>
2022-06-11 19:51:33 -0400

    Lab 2 done

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-#displaying the hostname information
-hostnamectl
-echo "Hostname: pc200464301"
-#displaying the domain name
-domainname
-#displaying the IP addresses
-hostname -I
-#displaying the storage information
-df -h
+
+#Output with blankline
+echo "========================="
+
+#Using cat command with embedded variables
+cat sysinfo.sh | echo '{print$2}'
+
+#displaying the hostname and router information in separate line
+cat /proc/sys/kernel/hostname
+hostname --fqdn
+
+#displaying operating system name and version 
+lsb_release -d
+
+#displaying the IP address
+ip route | grep default
+
+#displaying the root storage information
+df -h /
+
+echo "=========================="
