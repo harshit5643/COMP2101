5d5f4bcffd81ff535b65fd79406a6bff8655683b
harshit5643 <105835588+harshit5643@users.noreply.github.com>
2022-07-30 00:06:12 -0400

    Add files via upload

---
+++ Powershell Screenshots/Lab2.ps1
@@ -0,0 +1,15 @@
+﻿# Lab 2 COMP2101 welcome script for profile
+ls alias:np
+new-item -path alias:np -value notepad | out-null;
+function welcome {
+write-output "welcome to Harshit computer $env:computername Overload $env:username"
+$now = get-date -format 'HH:MM tt on dddd'
+write output according to "It is $now."
+};
+function get-cpuinfo {
+Get-Ciminstance -ClassName cim_processor | fl Manufacturer, Name, CurrentClockSpeed, MaxClockSpeed, NumberOfCorse
+};
+function get-mydisks {
+Get-PhysicalDisk | ft Manufacturer, Model, SerialNumber, FirmwareVersion, Size
+};
+Welcome.
