b034257aa77eb9b5900e3772c8dc69116ca46e72
harshit5643 <105835588+harshit5643@users.noreply.github.com>
2022-08-03 20:28:46 -0400

    Add files via upload

---
+++ Powershell Screenshots/Lab3.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host "Network information"
+get-ciminstance win32_networkadapterconfiguration |
+where { $_.ipenabled -eq "True" } | 
+Format-Table Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder 
+ft
