fdaf08e9c60fd01264ccf573c7a661c9ce6038c9
harshit5643 <105835588+harshit5643@users.noreply.github.com>
2022-08-17 16:39:51 -0400

    Add files via upload

---
+++ Powershell Screenshots/Lab 5.ps1
@@ -0,0 +1,28 @@
+﻿Param (
+    [switch]$Disks, [switch]$Network , [switch]$System
+)
+
+Write-Output "system details "
+if ($Disks -ne $true -and $Disks -ne $true -and $Network -ne $true) {
+   computersysteminfo
+   operatingsystem
+   processor
+   Ram
+   diskdrive
+   networkadapter
+   video
+}
+
+if ($Disks -eq $true) {
+    diskdrive
+}
+
+Write-Output "Network details"
+if($Network -eq 'true'-Or $Network -ne 'true'){
+   networkadapter
+}
+break
+
+"Output info"
+
+
